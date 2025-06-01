0a83b5487c5b9e6ef93d6ff64ef9f72555cfa4a6
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-06-01 13:42:48 +0300

    move monitoring service to a separate file

---
+++ cmd/service/monitor.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/lionpuro/trackcerts/certs"
+	"github.com/lionpuro/trackcerts/domain"
+	"github.com/lionpuro/trackcerts/model"
+	"github.com/lionpuro/trackcerts/notification"
+)
+
+type Monitor struct {
+	interval      time.Duration
+	domains       domain.Repository
+	notifications *notification.Service
+	quit          chan struct{}
+}
+
+func NewMonitor(interval time.Duration, dr domain.Repository, ns *notification.Service) *Monitor {
+	return &Monitor{
+		interval:      interval,
+		domains:       dr,
+		notifications: ns,
+		quit:          make(chan struct{}),
+	}
+}
+
+func (m *Monitor) Start() {
+	t := time.NewTicker(m.interval)
+	if err := m.poll(); err != nil {
+		log.Fatal(err)
+		return
+	}
+	for {
+		select {
+		case t := <-t.C:
+			start := time.Now()
+			log.Printf("start polling at %v", t)
+			if err := m.poll(); err != nil {
+				log.Printf("poll service: %v", err)
+			}
+			log.Printf("finish polling in %v", time.Since(start))
+		case <-m.quit:
+			log.Printf("stopping monitor...")
+			return
+		}
+	}
+}
+
+func (m *Monitor) poll() error {
+	domains, err := m.domains.All(context.Background())
+	if err != nil {
+		return err
+	}
+	workers := make(chan struct{}, 15)
+	wg := sync.WaitGroup{}
+	results := make(chan model.Domain, len(domains))
+
+	for _, dom := range domains {
+		wg.Add(1)
+		go func(d model.Domain) {
+			workers <- struct{}{}
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer func() {
+				<-workers
+				wg.Done()
+				cancel()
+			}()
+			cert, err := certs.FetchCert(ctx, d.DomainName)
+			if err != nil {
+				log.Printf("fetch certificate: %v", err)
+				return
+			}
+			domain := d
+			domain.Certificate = *cert
+			results <- domain
+		}(dom)
+	}
+
+	wg.Wait()
+	close(results)
+	return m.updateData(results)
+}
+
+func (m *Monitor) updateData(domainch chan model.Domain) error {
+	domains := make([]model.Domain, len(domainch))
+	i := 0
+	for d := range domainch {
+		domains[i] = d
+		i++
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	return m.domains.UpdateMultiple(ctx, domains)
+}
