34c90c03622ccd713bc2975958a4a9b23626327d
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-07-02 13:00:12 +0300

    respond with error page instead of the default error response

--- http/handlers_auth.go
+++ http/handlers_auth.go
@@ -17,11 +17,11 @@ func (h *Handler) LoginPage(w http.ResponseWriter, r *http.Request) {
 func (h *Handler) Logout(w http.ResponseWriter, r *http.Request) {
 	sess, err := h.AuthService.Session(r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		h.ErrorPage(w, r, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 	if err := sess.Delete(w, r); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		h.ErrorPage(w, r, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 
@@ -32,20 +32,20 @@ func (h *Handler) Login(a *auth.Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		state, err := auth.GenerateRandomState()
 		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			h.ErrorPage(w, r, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
 		sess, err := h.AuthService.Session(r)
 		if err != nil {
 			h.log.Error("failed to retrieve session", "error", err.Error())
-			http.Error(w, "", http.StatusInternalServerError)
+			h.ErrorPage(w, r, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
 		sess.SetState(state)
 		if err := sess.Save(w, r); err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			h.ErrorPage(w, r, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
@@ -59,13 +59,13 @@ func (h *Handler) AuthCallback(a *auth.Client) http.HandlerFunc {
 		sess, err := h.AuthService.Session(r)
 		if err != nil {
 			h.log.Error("failed to retrieve session", "error", err.Error())
-			http.Error(w, "", http.StatusInternalServerError)
+			h.ErrorPage(w, r, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
 		state, ok := sess.State()
 		if !ok || r.FormValue("state") != state {
-			http.Error(w, "Invalid state parameter.", http.StatusBadRequest)
+			h.ErrorPage(w, r, "Invalid state parameter", http.StatusBadRequest)
 			return
 		}
 
@@ -73,14 +73,14 @@ func (h *Handler) AuthCallback(a *auth.Client) http.HandlerFunc {
 		tkn, err := a.ExchangeToken(r.Context(), code)
 		if err != nil {
 			h.log.Error("failed to exchange token", "error", err.Error())
-			http.Error(w, "Bad request", http.StatusBadRequest)
+			h.ErrorPage(w, r, "Bad request", http.StatusBadRequest)
 			return
 		}
 
 		idToken, err := a.VerifyToken(r.Context(), tkn)
 		if err != nil {
 			h.log.Error("failed to verify token", "error", err.Error())
-			http.Error(w, "Bad request", http.StatusBadRequest)
+			h.ErrorPage(w, r, "Bad request", http.StatusBadRequest)
 			return
 		}
 
@@ -90,19 +90,19 @@ func (h *Handler) AuthCallback(a *auth.Client) http.HandlerFunc {
 		}
 		if err := idToken.Claims(&user); err != nil {
 			h.log.Error("failed to unmarshal token claims", "error", err.Error())
-			http.Error(w, "Bad request", http.StatusBadRequest)
+			h.ErrorPage(w, r, "Bad request", http.StatusBadRequest)
 			return
 		}
 
 		if err := h.UserService.Create(user.ID, user.Email); err != nil {
 			h.log.Error("failed to create user", "error", err.Error())
-			http.Error(w, "Error creating user", http.StatusInternalServerError)
+			h.ErrorPage(w, r, "Something went wrong", http.StatusInternalServerError)
 			return
 		}
 		sess.SetUser(model.User{ID: user.ID, Email: user.Email})
 		if err := sess.Save(w, r); err != nil {
 			h.log.Error("failed to save session", "error", err.Error())
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			h.ErrorPage(w, r, "Something went wrong", http.StatusInternalServerError)
 			return
 		}
 
