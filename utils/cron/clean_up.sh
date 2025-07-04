7c7a39f916011a7b30a1874233639c1a9d1d9df5
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-07-04 08:54:14 +0300

    rename cleanup script

--- utils/cron/clean_up.sh
+++ utils/cron/clean_up.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-psql "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_DOCKER_PORT}/${POSTGRES_DB}?sslmode=disable" -f clean-up-notifications.sql
+psql "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_DOCKER_PORT}/${POSTGRES_DB}?sslmode=disable" -f clean_up.sql
