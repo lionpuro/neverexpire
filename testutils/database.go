a081339ff6e619439a20612fa77537cd5abb09ac
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-07-21 18:40:42 +0300

    populate test database with generated users and hosts

--- testutils/database.go
+++ testutils/database.go
@@ -2,6 +2,7 @@ package testutils
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -9,7 +10,10 @@ import (
 	"strings"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/lionpuro/neverexpire/logging"
+	"github.com/lionpuro/neverexpire/users"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -62,5 +66,98 @@ func NewDatabase() (conn *pgxpool.Pool, cleanup func() error, err error) {
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to initialize pool: %v", err)
 	}
+	// populate with test data
+	if err := insertTestData(pool); err != nil {
+		return nil, nil, fmt.Errorf("failed to populate db with test data: %v", err)
+	}
 	return pool, clean, nil
+}
+
+func insertTestData(conn *pgxpool.Pool) error {
+	ctx := context.Background()
+	var usrs []users.User
+	for range 2 {
+		user, err := NewTestUser()
+		if err != nil {
+			return err
+		}
+		usrs = append(usrs, user)
+	}
+	hosts, err := NewTestHosts()
+	if err != nil {
+		return err
+	}
+
+	for _, user := range usrs {
+		tx, err := conn.Begin(ctx)
+		defer func() {
+			if err := tx.Rollback(ctx); err != nil && !errors.Is(err, pgx.ErrTxClosed) {
+				logging.DefaultLogger().Error("failed to rollback tx", "error", err.Error())
+			}
+		}()
+		if err != nil {
+			return err
+		}
+		_, err = tx.Exec(ctx, `INSERT INTO users (id, email) VALUES ($1, $2)`, user.ID, user.Email)
+		if err != nil {
+			return err
+		}
+
+		for _, h := range hosts {
+			var id int
+			var errStr *string = nil
+			if h.Certificate.Error != nil {
+				str := h.Certificate.Error.Error()
+				errStr = &str
+			}
+			sql := `
+			INSERT INTO hosts (
+				hostname,
+				dns_names,
+				ip_address,
+				issued_by,
+				status,
+				expires_at,
+				checked_at,
+				latency,
+				signature,
+				error_message
+			)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+			ON CONFLICT (hostname) DO UPDATE SET
+				dns_names      = EXCLUDED.dns_names,
+				ip_address     = EXCLUDED.ip_address,
+				issued_by      = EXCLUDED.issued_by,
+				status         = EXCLUDED.status,
+				expires_at     = EXCLUDED.expires_at,
+				checked_at     = EXCLUDED.checked_at,
+				latency        = EXCLUDED.latency,
+				signature      = EXCLUDED.signature,
+				error_message  = EXCLUDED.error_message
+			RETURNING id`
+			err := tx.QueryRow(ctx, sql,
+				h.Hostname,
+				h.Certificate.DNSNames,
+				h.Certificate.IP,
+				h.Certificate.IssuedBy,
+				h.Certificate.Status,
+				h.Certificate.ExpiresAt,
+				h.Certificate.CheckedAt,
+				h.Certificate.Latency,
+				h.Certificate.Signature,
+				errStr,
+			).Scan(&id)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(ctx, `INSERT INTO user_hosts (host_id, user_id) VALUES ($1, $2)`, id, user.ID)
+			if err != nil {
+				return err
+			}
+		}
+		if err := tx.Commit(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- testutils/testdata.go
+++ testutils/testdata.go
@@ -3,7 +3,10 @@ package testutils
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"fmt"
+	"time"
 
+	"github.com/lionpuro/neverexpire/hosts"
 	"github.com/lionpuro/neverexpire/users"
 )
 
@@ -23,4 +26,82 @@ func NewTestUser() (users.User, error) {
 	email := id[:8] + "@example.com"
 	user := users.User{ID: id, Email: email}
 	return user, nil
+}
+
+func NewTestHost(exp *time.Time, status hosts.CertificateStatus, hostErr error) (hosts.Host, error) {
+	str, err := RandomString(8)
+	if err != nil {
+		return hosts.Host{}, err
+	}
+	name := str + ".example.com"
+	host := hosts.Host{
+		Hostname: name,
+		Certificate: hosts.CertificateInfo{
+			DNSNames:  name,
+			IP:        "",
+			IssuedBy:  "Example Certs",
+			ExpiresAt: exp,
+			Status:    status,
+			Latency:   1,
+			Signature: str,
+			Error:     hostErr,
+		},
+	}
+	return host, nil
+}
+
+func NewTestHosts() ([]hosts.Host, error) {
+	input := []struct {
+		expires *time.Time
+		status  hosts.CertificateStatus
+		err     error
+	}{
+		// healthy
+		{
+			expires: addTime(50 * 24 * time.Hour),
+			status:  hosts.CertificateStatusHealthy,
+		},
+		{
+			expires: addTime(10 * 24 * time.Hour),
+			status:  hosts.CertificateStatusHealthy,
+		},
+		{
+			expires: addTime(5 * 24 * time.Hour),
+			status:  hosts.CertificateStatusHealthy,
+		},
+		{
+			expires: addTime(20 * time.Hour),
+			status:  hosts.CertificateStatusHealthy,
+		},
+		// invalid
+		{
+			expires: addTime(62 * 24 * time.Hour),
+			status:  hosts.CertificateStatusInvalid,
+			err:     hosts.ErrCertInvalid,
+		},
+		{
+			expires: nil,
+			status:  hosts.CertificateStatusInvalid,
+			err:     hosts.ErrCertInvalid,
+		},
+		// offline
+		{
+			expires: nil,
+			status:  hosts.CertificateStatusOffline,
+		},
+	}
+	var result []hosts.Host
+	for _, in := range input {
+		h, err := NewTestHost(in.expires, in.status, in.err)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create fake host data: %v", err)
+		}
+		result = append(result, h)
+	}
+	return result, nil
+}
+
+func addTime(d time.Duration) *time.Time {
+	t := time.Now().UTC().Add(d)
+	return &t
 }
