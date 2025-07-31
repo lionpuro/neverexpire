5e30d81e722f096c31b3914f9f4329b036bd1717
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-07-31 09:29:50 +0300

    separate method from pattern in route registration function

--- web/routes.go
+++ web/routes.go
@@ -7,8 +7,8 @@ import (
 func NewRouter(h *Handler) *http.ServeMux {
 	r := http.NewServeMux()
 
-	handle := func(p string, hf http.HandlerFunc) {
-		r.Handle(p, h.Authenticate(
+	handle := func(method, p string, hf http.HandlerFunc) {
+		r.Handle(method+" "+p, h.Authenticate(
 			http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				w.Header().Set("Content-Type", "text/html; charset=utf-8")
 				hf(w, r)
@@ -16,26 +16,26 @@ func NewRouter(h *Handler) *http.ServeMux {
 		))
 	}
 
-	handle("GET /", h.HomePage)
-	handle("GET /hosts", h.RequireAuth(h.HostsPage))
-	handle("GET /hosts/new", h.RequireAuth(h.NewHostsPage))
-	handle("POST /hosts", h.RequireAuth(h.CreateHosts))
-	handle("GET /hosts/{id}", h.RequireAuth(h.HostPage))
-	handle("DELETE /hosts/{id}", h.RequireAuth(h.DeleteHost))
-	handle("GET /notifications", h.RequireAuth(h.NotificationsPage))
-	handle("GET /partials/notifications/count", h.RequireAuth(h.NotificationsCount))
-	handle("PATCH /notifications/read", h.RequireAuth(h.ReadNotifications))
-	handle("GET /login", h.LoginPage)
-	handle("GET /logout", h.Logout)
-	handle("DELETE /account", h.RequireAuth(h.DeleteAccount))
-	handle("GET /settings", h.RequireAuth(h.SettingsPage))
-	handle("PUT /settings/reminders", h.RequireAuth(h.UpdateReminders))
-	handle("POST /settings/webhook", h.RequireAuth(h.AddWebhook))
-	handle("DELETE /settings/webhook", h.RequireAuth(h.DeleteWebhook))
-	handle("GET /account/api", h.RequireAuth(h.APIPage))
-	handle("GET /account/tokens/new", h.RequireAuth(h.CreateAPIKey))
-	handle("DELETE /account/tokens/{id}", h.RequireAuth(h.DeleteAPIKey))
-	handle("GET /demo/hosts", h.HostsDemoPage)
+	handle("GET", "/", h.HomePage)
+	handle("GET", "/hosts", h.RequireAuth(h.HostsPage))
+	handle("GET", "/hosts/new", h.RequireAuth(h.NewHostsPage))
+	handle("POST", "/hosts", h.RequireAuth(h.CreateHosts))
+	handle("GET", "/hosts/{id}", h.RequireAuth(h.HostPage))
+	handle("DELETE", "/hosts/{id}", h.RequireAuth(h.DeleteHost))
+	handle("GET", "/notifications", h.RequireAuth(h.NotificationsPage))
+	handle("GET", "/partials/notifications/count", h.RequireAuth(h.NotificationsCount))
+	handle("PATCH", "/notifications/read", h.RequireAuth(h.ReadNotifications))
+	handle("GET", "/login", h.LoginPage)
+	handle("GET", "/logout", h.Logout)
+	handle("DELETE", "/account", h.RequireAuth(h.DeleteAccount))
+	handle("GET", "/settings", h.RequireAuth(h.SettingsPage))
+	handle("PUT", "/settings/reminders", h.RequireAuth(h.UpdateReminders))
+	handle("POST", "/settings/webhook", h.RequireAuth(h.AddWebhook))
+	handle("DELETE", "/settings/webhook", h.RequireAuth(h.DeleteWebhook))
+	handle("GET", "/account/api", h.RequireAuth(h.APIPage))
+	handle("GET", "/account/tokens/new", h.RequireAuth(h.CreateAPIKey))
+	handle("DELETE", "/account/tokens/{id}", h.RequireAuth(h.DeleteAPIKey))
+	handle("GET", "/demo/hosts", h.HostsDemoPage)
 	r.HandleFunc("GET /auth/google/login", h.Login(h.Authenticator.GoogleClient))
 	r.HandleFunc("GET /auth/google/callback", h.AuthCallback(h.Authenticator.GoogleClient))
 	r.Handle("GET /assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("assets/public"))))
