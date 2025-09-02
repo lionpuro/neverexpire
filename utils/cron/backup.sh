34b707df39949c8906992071bae9b52087eaa1fb
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-09-02 16:04:35 +0300

    refactor database backup script
    Configure Rclone using environment variables instead of the config file.

--- utils/cron/backup.sh
+++ utils/cron/backup.sh
@@ -10,25 +10,7 @@ if [ ! -f "$DUMP_PATH" ]; then
 	exit 1
 fi
 
-RCLONE_CONFIG_CONTENT=$(cat <<EOL
-[r2]
-type = s3
-provider = Cloudflare
-access_key_id = ${R2_ACCESS_KEY_ID}
-secret_access_key = ${R2_SECRET_ACCESS_KEY}
-region = auto
-endpoint = ${R2_ENDPOINT}
-acl = private
-no_check_bucket = true
-EOL
-)
-
-mkdir -p /opt/rclone
-RCLONE_CONFIG="/opt/rclone/rclone.conf"
-touch "$RCLONE_CONFIG"
-echo "$RCLONE_CONFIG_CONTENT" > "$RCLONE_CONFIG"
-
-rclone copy --config="$RCLONE_CONFIG" $DUMP_PATH r2:"${R2_BUCKET_NAME}/${POSTGRES_DB}"
+rclone copy $DUMP_PATH r2:"${R2_BUCKET_NAME}/${POSTGRES_DB}"
 
 if [ $? -eq 0 ]; then
 	echo "Backup of database ${POSTGRES_DB} successfully uploaded to R2"
